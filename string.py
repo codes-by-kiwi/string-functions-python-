8d1b959cbf1e1bac1868be94bbdab9398de539ca
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-04-01 13:49:23 +0200

    Add files via upload

---
+++ string.py
@@ -0,0 +1,5 @@
+itsafunstring = "Hello. I am currently reading Jane Austen."
+print(itsafunstring.upper()) #converts it to upper case
+print(itsafunstring.lower()) #converts it to lower case
+print(itsafunstring.startswith("Hello")) #checks if string starts with hello
+print(itsafunstring.endswith("books")) #checks if string ends with books
